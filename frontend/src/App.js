1ef938dd585b05f3029970291bc25b8f21755fd4
liliaferrouk <ferrouklilia@gmail.com>
2023-12-29 17:04:54 +0100

    made some changes
    tomorow: -solve the prob
    - delete btn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import React from 'react';
 
 function App() {
   const [tasks,setTasks] = React.useState([])
+
   React.useEffect(()=>{
     fetch(`http://127.0.0.1:8000/api/tasks`)
     .then(res=>res.json())
     .then(data => setTasks(data))
   },[])
+  //je doit trouver une solution s that the page will be rundered a chaque fois qu'un changement arrive
 
   function addTask(notename){
     fetch(`http://127.0.0.1:8000/api/tasks`,{
@@ -23,10 +25,24 @@ function App() {
       return [...prev, {note:notename, done:false}]
     })
   }
+
+  function getmessage(){
+    const per = tasks.filter(t=> t.done).length / tasks.length *100
+    console.log(per)
+    if (per===0) {
+      return("Try doing at least one 🥹")
+    }else if(per===100){
+      return("Nice job for today ⛱️")
+    }else{
+      return("Keep it going 💪🏻!")
+    }
+  }
+
   return (
     <main>
+      <h1>{tasks.filter(t=> t.done).length}/{tasks.length} Complete</h1>
+      <h2>{getmessage()}</h2>
       <TaskForm onadd={addTask} />
-      <p>{tasks.length}</p>
       {tasks.map((task,index)=>(
         <Task key={index} {...task} />
       ))}
